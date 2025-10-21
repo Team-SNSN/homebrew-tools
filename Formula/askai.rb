e003ed7d6bb6374a33f1f01450d3e64f35a7284f
ahnjh05141 <jaehyuns05141@gmail.com>
2025-10-21 15:25:22 +0900

    test: v0.1.0

--- Formula/askai.rb
+++ Formula/askai.rb
@@ -1,8 +1,8 @@
 class Askai < Formula
     desc "Natural language to shell command helper"
-    homepage "https://github.com/Team-SNSN/askai/release"
+    homepage "https://github.com/Team-SNSN/askai"
     url "https://github.com/Team-SNSN/askai/archive/refs/tags/v0.1.0.tar.gz"
-    sha256 "80630e3974ee6452ef4857d67f7982b69b55a495be440ceab7e81be48c55c560"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     license "MIT"
     version "0.1.0"
 
