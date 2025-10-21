f74629342d6f127fa8451c7118795c88ed3c3c92
ahnjh05141 <jaehyuns05141@gmail.com>
2025-10-21 14:49:56 +0900

    fix: sha256 hash

--- Formula/askai.rb
+++ Formula/askai.rb
@@ -2,7 +2,7 @@ class Askai < Formula
     desc "Natural language to shell command helper"
     homepage "https://github.com/Team-SNSN/askai"
     url "https://github.com/Team-SNSN/askai/archive/refs/tags/v0.1.0.tar.gz"
-    sha256 "b16b11c17f41f9ac208147b8c19e9d80502d518d183458fa59fe5c18e290c505  /tmp/askai-0.1.0.tar.gz"
+    sha256 "b16b11c17f41f9ac208147b8c19e9d80502d518d183458fa59fe5c18e290c505"
     license "MIT"
     version "0.1.0"
 
